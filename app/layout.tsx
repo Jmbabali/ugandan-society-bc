e9d51f8c390565c77cba69fcb40cb56123308856
Jmbabali <jon12_cena@rocketmail.com>
2026-07-20 23:20:12 -0700

    fix google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import SiteShell from "./components/SiteShell";
+import Script from "next/script";
 
 export const metadata = {
   title: "Ugandan Society in BC",
@@ -15,6 +16,19 @@ export default function RootLayout({
     <html lang="en" data-scroll-behavior="smooth">
       <body>
         <SiteShell>{children}</SiteShell>
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-XY5JWEF73X"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-XY5JWEF73X');
+  `}
+</Script>
       </body>
     </html>
   );
